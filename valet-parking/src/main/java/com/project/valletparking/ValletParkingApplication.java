8fb3849ecbcb42aaac9b9bc2c648fd6a308bb0c6
amnsuryansh <princesuryansh2000@gmail.com>
2025-05-25 10:02:22 +0530

    struct: added structure of project

---
+++ valet-parking/src/main/java/com/project/valletparking/ValletParkingApplication.java
@@ -0,0 +1,13 @@
+package com.project.valletparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValletParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValletParkingApplication.class, args);
+    }
+
+}
---
+++ valet-parking/src/test/java/com/project/valletparking/ValletParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.valletparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValletParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
